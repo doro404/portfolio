08fe6dd6d232450574253eb17ad83c200d6afa46
doro404 <166941040+doro404@users.noreply.github.com>
2025-04-19 16:38:22 -0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,13 @@
+// config.js
+export const tag_site = {
+    title: "meu site",
+    description: 'description',
+    favicon: "/img/logo.png",
+};
+  
+export const config = {
+whats_link: "https://wa.me/5511999999",
+telegram_link: "https://t.me/seuUsuario",
+mail_endereço: "mailto:seuemail@exemplo.com",
+name: "@ 2025 Paulo Henrique",
+};
